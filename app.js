534c7615eeb062630ccb2a3bc2eae1ee504b830b
Max <kolotseymax@gmail.com>
2019-12-23 02:47:04 +0300

    Hello World

--- app.js
+++ app.js
@@ -6,3 +6,5 @@ let today = new Date();
 let formateDate = today.toDateString();
 let selectElement = document.getElementById('date');
 selectElement.innerHTML = formateDate;
+
+alert("Hello World!");
