fb7cc34d3ddf2d5bc748124bf8c435785103db1e
Max <kolotseymax@gmail.com>
2020-02-25 20:23:43 +0300

    App.js commit

--- app.js
+++ app.js
@@ -1,8 +1,32 @@
 'use strict';
 
-console.log('init log');
+let li_load = document.querySelector('#load');
+let li_clear = document.querySelector('#clear');
 
-let today = new Date();
-let formateDate = today.toDateString();
-let selectElement = document.getElementById('date');
-selectElement.innerHTML = formateDate;
+li_load.addEventListener('click',loadData);
+li_clear.addEventListener('click',clearData);
+function createParagraph(posts){
+    let body = document.querySelector('#container');
+    posts.forEach(post => {
+        let div = document.createElement('div');
+        div.classList.add('post');
+        let paragraph = document.createElement('p');
+        let h3 = document.createElement('h3');
+        h3.innerText = post.title;
+        paragraph.innerText = post.body;
+        div.appendChild(h3);
+        div.appendChild(paragraph);
+        body.appendChild(div);
+    });
+}
+
+async function loadData(){
+const url ='https://jsonplaceholder.typicode.com/users/1/posts';
+let response = await fetch(url);
+let posts = await response.json();
+createParagraph(posts);
+}
+function clearData(){
+    let body = document.querySelector('#container');
+    body.querySelectorAll('div').forEach(el=>body.removeChild(el));
+}
